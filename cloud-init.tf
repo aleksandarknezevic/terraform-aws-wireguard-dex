9c7f1e90186b5381bbf6555dcd4e53f6b38c8b37
aleksandarknezevic <perun.84@gmail.com>
2022-01-12 17:02:56 +0100

    indent fix

--- cloud-init.tf
+++ cloud-init.tf
@@ -16,16 +16,16 @@ data "template_cloudinit_config" "config" {
         - name: ubuntu
           groups: docker
       write_files:
-        - path: /home/ubuntu/wireguard/wg-config.yaml
-          encoding: b64
-          content: ${var.wg_access_server_config}
-          owner: ubuntu:ubuntu
-          permissions: '0644'
-        - path: /home/ubuntu/wireguard/docker-compose.yaml
-          encoding: b64
-          content: ${var.docker_compose_file}
-          owner: ubuntu:ubuntu
-          permissions: '0644'
+      - path: /home/ubuntu/wireguard/wg-config.yaml
+        encoding: b64
+        content: ${var.wg_access_server_config}
+        owner: ubuntu:ubuntu
+        permissions: '0644'
+      - path: /home/ubuntu/wireguard/docker-compose.yaml
+        encoding: b64
+        content: ${var.docker_compose_file}
+        owner: ubuntu:ubuntu
+        permissions: '0644'
       runcmd:
         - [ cd, /home/ubuntu/wireguard ]
         - [ docker-compose, up, -d ]
